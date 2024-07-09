52ef0abdf380e0bfcc69398c26dc36e35469624d
Andy <huylv.dev@gmail.com>
2024-07-09 11:40:40 +0700

    add code

--- src/app/anh/page.tsx
+++ src/app/anh/page.tsx
@@ -2,13 +2,15 @@
 import html2canvas from "html2canvas";
 import { useSearchParams } from "next/navigation";
 import { Suspense, useEffect, useState } from "react";
+import { useQRCode } from "next-qrcode";
 
 export default function Home() {
   const [done, setDone] = useState(false);
+  const { SVG } = useQRCode();
   let load = false;
   useEffect(() => {
     setTimeout(() => {
-      takeScreenshot();
+      // takeScreenshot();
     }, 1000);
   }, [load]);
 
@@ -69,32 +71,117 @@ export default function Home() {
     const [avatar] = searchParams.getAll("avatar") || "";
 
     return (
-      <div className=' w-[400px] h-[600px] flex items-center flex-col bg-slate-500 relative' id='capture'>
-        <img src='/anhnen.png' alt='Sample Image' className=' w-[400px] h-[600px]' />
+      <div className='flex flex-col  h-full w-[400px]   '>
+        <div className='  grid grid-cols-6  bg-[#1E6FA2] text-white items-center  pb-5 px-4 text  '>
+          <img src='/icon.svg' alt='Sample Image' className=' col-span-1  max-w-[50px] mt-4   ' />
 
-        <img src={avatar} alt='Sample Image' className='w-[110px] h-[150px]  absolute top-[285px] left-[30px] ' />
-
-        <div className='flex flex-col self-center justify-start items-center absolute top-[365px] left-[150px]  w-[210px] gap-y-2 '>
-          <span className=' text-[#00428A] uppercase  font-black  text-[15px] '>{name}</span>
-          <span className='  uppercase text-[#00428A] font-black  self-center text-center text-[15px]'>{delegate}</span>
+          <span className='  mt-5 col-span-3  text-sm  font-bold  '>UỶ BAN MTTQ VIỆT NAMTHÀNH PHỐ HÀ NỘI</span>
+          <button
+            onClick={() => takeScreenshot()}
+            className='  mt-5  col-span-2 text-center  p-2 rounded-lg font-semibold uppercase  bg-white text-xs  text-red-600  '
+          >
+            Tải thẻ đại biểu
+          </button>
         </div>
+        <div className='flex flex-col '>
+          <span className=' uppercase text-center  text-red-600 font-bold'>Chào mừng đại biểu</span>
+          <div className='flex shadow-lg rounded-lg justify-around   py-10 my-8  items-center    h-40'>
+            <div className='w-[30%]  '>
+              <SVG
+                text={"https://github.com/bunlong/next-qrcode"}
+                options={{
+                  width: 120,
+
+                  color: {
+                    dark: "#010599FF",
+                    light: "#ffffff",
+                  },
+                }}
+              />
+            </div>
+
+            <div className='flex flex-col   items-start gap-y-1'>
+              <span className='  font-bold text-xl'>Ông NGUYỄN ĐẮC TÌNH</span>
+              <div className='flex items-center'>
+                <div className=' mx-2 w-2 h-2 bg-black' />
+                <span className=' font-light'>
+                  Mã đại biểu: <span className='font-semibold'>001938</span>
+                </span>
+              </div>
+
+              <div className='flex items-center'>
+                <div className=' mx-2 w-2 h-2 bg-black' />
+                <span className=' font-light'>
+                  Đoàn đại biểu: <span className='font-semibold'>Hoài Đức</span>
+                </span>
+              </div>
+
+              <div className='flex items-center'>
+                <div className=' mx-2 w-2 h-2 bg-black' />
+                <span className=' font-light'>
+                  Chức vụ: <span className='font-semibold'>Uỷ viên</span>
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className=' w-[400px] h-[600px] flex items-center flex-col  relative' id='capture'>
+          <img src='/anhnen.jpg' alt='Sample Image' className=' w-[400px] h-[600px]  rounded-[10px]' />
+
+          <img src='/Anh34.jpg' alt='Sample Image' className='w-[98px] h-[147px]  absolute top-[287px] left-[36px] ' />
+
+          <div className='absolute w-[70px] h-[70]  top-[440px] left-[45px]'>
+            <SVG
+              text={"https://github.com/bunlong/next-qrcode"}
+              options={{
+                margin: 2,
+                width: 70,
+                color: {
+                  dark: "#010599FF",
+                  light: "#ffffff",
+                },
+              }}
+            />
+          </div>
+          <div className='flex flex-col self-center justify-start items-center absolute top-[365px] left-[150px]  w-[210px] '>
+            <span className=' text-[#3760AE] uppercase  font-black  text-[15px] '>{name}</span>
+            <span className='  uppercase text-[#3760AE] font-black  self-center text-center text-[15px]'>
+              {delegate}
+            </span>
+          </div>
+
+          <div className='flex flex-col self-center justify-start items-center absolute top-[455px] left-[150px]  w-[210px] '>
+            <span className=' text-[#3760AE] uppercase  font-black  text-[15px] '>Mã đại biểu : 00156572</span>
+          </div>
+        </div>
+
+        <button
+          className='px-4 py-3  uppercase  bg-[#1E6FA2] rounded-md mt-2  text-center self-center text-white  font-bold  w-full  '
+          onClick={takeScreenshot}
+        >
+          Tải thẻ đại biểu
+        </button>
       </div>
     );
   }
 
   return (
-    <>
-      <div className=' flex w-screen h-screen justify-center'>
-        <Suspense>
+    <div className=' flex   justify-center'>
+      <Suspense>
+        <div className='flex flex-col w-[400px]'>
           <Search />
-        </Suspense>
-      </div>
-      <button
-        className='px-4 py-2   text-center self-center text-blue-600  font-bold  w-full  rounded'
-        onClick={takeScreenshot}
-      >
-        Lưu ảnh về máy
-      </button>
-    </>
+          <div className='px-4 py-3  uppercase  bg-[#1E6FA2] mt-2 flex flex-col  text-center self-center text-white  font-bold    '>
+            <img src='/icon.svg' alt='Sample Image' className='w-[98px] self-center  ' />
+
+            <span className='  mt-5 px-8  '>
+              BAN TỔ CHỨC ĐẠI HỘI ĐẠI BIỂU MẶT TRẬN TỔ QUỐC VIỆT NAM THÀNH PHỐ HÀ NỘI LẦN THỨ XVIII NHIỆM KỲ 2024 - 2029
+            </span>
+          </div>
+
+          <div className='px-4 py-3  uppercase  bg-[#1E6FA2]  mt-[2px] flex flex-col  text-center self-center w-full h-[20px] text-white  font-bold    '></div>
+        </div>
+      </Suspense>
+    </div>
   );
 }
