469144188d89407231e664e6afcea505ece474c7
Andy <huylv.dev@gmail.com>
2024-07-10 11:37:19 +0700

    add wellknow

--- src/app/client/post-news/page.tsx
+++ src/app/client/post-news/page.tsx
@@ -11,6 +11,8 @@ const QuillEditor = dynamic(() => import("react-quill"), { ssr: false });
 
 const Profile: NextPage<any> = () => {
   const [content, setContent] = useState("");
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
   const quillModules = {
     toolbar: [
       [{ header: "1" }, { header: "2" }, { header: [3, 4, 5, 6, false] }],
@@ -55,6 +57,10 @@ const Profile: NextPage<any> = () => {
     "code-block",
   ];
 
+  const handleSubmit = () => {
+    console.log("handleSubmithandleSubmithandleSubmit", title);
+  };
+
   const handleEditorChange = (newContent: any) => {
     setContent(newContent);
   };
@@ -67,7 +73,11 @@ const Profile: NextPage<any> = () => {
           <div className=' h-lvh    rounded-xl '>
             <div className='flex flex-col justify-between   '>
               <p className=' font-workSansMedium text-lg'>Tiêu đề</p>
-              <input className='w-[50%] border h-12 rounded-md px-2 mt-2' placeholder={"Tiêu đề bài viết"}></input>
+              <input
+                onChange={(e) => setTitle(e.target.value)}
+                className='w-[50%] border h-12 rounded-md px-2 mt-2'
+                placeholder={"Tiêu đề bài viết"}
+              ></input>
             </div>
 
             <div className='flex flex-col justify-between  mt-4  '>
@@ -75,13 +85,26 @@ const Profile: NextPage<any> = () => {
               <input className='w-[50%] border h-12 rounded-md px-2 mt-2' placeholder={"Mô tả bài viết"}></input>
             </div>
 
-            <QuillEditor
-              value={content}
-              onChange={handleEditorChange}
-              modules={quillModules}
-              formats={quillFormats}
-              className='w-full h-[70%] mt-10 bg-white'
-            />
+            <div className='flex  flex-col h-screen '>
+              <QuillEditor
+                value={content}
+                onChange={handleEditorChange}
+                modules={quillModules}
+                formats={quillFormats}
+                className='w-full h-40   mt-10 bg-white'
+              />
+
+              <button
+                className='w-[100%]'
+                onClick={() => {
+                  handleSubmit();
+                }}
+              >
+                <p className=' bg-primary-500 font-extrabold text-lg border z-50  px-8 py-2 border-slate-400 rounded-md flex items-center justify-center text-white	'>
+                  Đăng xuất
+                </p>
+              </button>
+            </div>
           </div>
         </div>
       </div>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <ContextConsumer>
           <QueryClientProvider client={queryClient}>
             {children}
+
             <SideMenu />
             <ToastContainer
               position='top-right'
