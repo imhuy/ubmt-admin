81fe80284a1458d7de208c0a7f82a2b589d6c3d1
Andy <huylv.dev@gmail.com>
2024-07-16 10:55:53 +0700

    add code

--- src/app/client/contact/page.tsx
+++ src/app/client/contact/page.tsx
@@ -51,51 +51,38 @@ const Rechange: NextPage<any> = () => {
 
   return (
     <AppLayout>
-      <div className="w-full h-screen flex flex-col mb-20">
-        <div className="p-6">
+      <div className='w-full h-screen flex flex-col mb-20'>
+        <div className='p-6'>
           <Header />
-          <div className="h-[1px] bg-black bg-opacity-20 my-4 max-lg:hidden" />
+          <div className='h-[1px] bg-black bg-opacity-20 my-4 max-lg:hidden' />
         </div>
-        <div className="flex w-full justify-center  ">
-          <div className=" grid grid-cols-3  w-[95%] max-sm:grid-cols-1 max-md:grid-cols-2 gap-8   leading-6  ">
+        <div className='flex w-full justify-center  '>
+          <div className=' grid grid-cols-3  w-[95%] max-sm:grid-cols-1 max-md:grid-cols-2 gap-8   leading-6  '>
             {lstTop.map((item, i) => (
-              <div
-                key={i}
-                className={`flex  ${item.className}  flex-col  gap-y-3  items-center p-6 rounded-lg`}
-              >
+              <div key={i} className={`flex  ${item.className}  flex-col  gap-y-3  items-center p-6 rounded-lg`}>
                 <Image
                   src={Contact2Icon}
                   width={80}
                   height={80}
-                  alt="home-bg"
-                  className=" size-28 bg-white  rounded-full p-2 mr-2"
+                  alt='home-bg'
+                  className=' size-28 bg-white  rounded-full p-2 mr-2'
                 />
 
-                <p className=" font-workSansMedium">{item.name}</p>
-                <p className=" font-workSansSemiBold  text-2xl text-green-800">
-                  {item.description}
-                </p>
-                <p className="text-sm font-workSansMedium">{item.note}</p>
+                <p className=' font-workSansMedium'>{item.name}</p>
+                <p className=' font-workSansSemiBold  text-2xl text-green-800'>{item.description}</p>
+                <p className='text-sm font-workSansMedium'>{item.note}</p>
 
-                <div className=" w-[100%] bg-blue-500 py-3 mt-2 rounded-md flex justify-center items-center gap-x-2  ">
-                  <div className="   size-4">
-                    <Icon color=" white" className="" icon="ri-phone-fill" />
+                <div className=' w-[100%] bg-blue-500 py-3 mt-2 rounded-md flex justify-center items-center gap-x-2  '>
+                  <div className='   size-4'>
+                    <Icon color=' white' className='' icon='ri-phone-fill' />
                   </div>
-                  <span className=" text-white  font-workSansSemiBold ">
-                    {" "}
-                    Gọi ngay
-                  </span>
+                  <span className=' text-white  font-workSansSemiBold '> Gọi ngay</span>
                 </div>
               </div>
             ))}
           </div>
         </div>
       </div>
-      <CopyModal
-        title={title}
-        isOpen={isOpenInfo}
-        closeModal={() => setIsOpenInfo(false)}
-      />
     </AppLayout>
   );
 };
