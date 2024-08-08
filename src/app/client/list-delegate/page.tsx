a37836f973d12906110ec218565788577948ad7e
Andy <huylv.dev@gmail.com>
2024-08-08 14:07:35 +0700

    add code

--- src/app/client/list-delegate/page.tsx
+++ src/app/client/list-delegate/page.tsx
@@ -152,8 +152,8 @@ const ListDelegate: NextPage<any> = () => {
 
                           <Link
                             href={{
-                              // pathname: `https://daihoi.mttqhanoi.org.vn`,
-                              pathname: `http://localhost:3000`,
+                              pathname: `https://daihoi.mttqhanoi.org.vn`,
+                              // pathname: `http://localhost:3000`,
                               query: { id: item.code },
                             }}
                             target='_blank'
