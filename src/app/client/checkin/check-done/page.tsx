27781da5e12989a0f1eab359105a0523e523bf6b
Andy <huylv.dev@gmail.com>
2024-08-21 13:11:21 +0700

    add code

--- src/app/client/checkin/check-done/page.tsx
+++ src/app/client/checkin/check-done/page.tsx
@@ -24,7 +24,7 @@ export default function Home() {
       getData(intervalId);
       const newTimeoutId = setTimeout(() => {
         setIsWaiting(true);
-      }, 10000); // 10 giây
+      }, 20000); // 10 giây
       setTimeoutId(newTimeoutId);
       return () => clearInterval(intervalId);
     }, [listHistory.data]);
